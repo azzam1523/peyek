1357451905339fb5c9c2adac31045e75e0798de8
az15 <mustamidgebang@gmail.com>
2026-05-06 08:18:18 +0700

    Force landscape mode and fit screen for mobile devices

--- src/main.ts
+++ src/main.ts
@@ -35,10 +35,11 @@ const config: Phaser.Types.Core.GameConfig = {
     height: 720,
     parent: 'app',
     scale: {
-        mode: Phaser.Scale.EXPAND,
+        mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1280,
-        height: 720
+        height: 720,
+        expandParent: true
     },
     physics: {
         default: 'arcade',
