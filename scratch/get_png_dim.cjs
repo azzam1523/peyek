78e48f039429c8e95495663158bd6a7ea2e3dcbc
az15 <mustamidgebangu@gmail.com>
2026-05-06 07:14:54 +0700

    chore: remove unnecessary files and update .gitignore

--- scratch/get_png_dim.cjs
+++
@@ -1,6 +0,0 @@
-
-const fs = require('fs');
-const buffer = fs.readFileSync('d:\\Test\\tembak_ikan\\public\\assets\\coolfish\\sharkjumbo.png');
-const width = buffer.readUInt32BE(16);
-const height = buffer.readUInt32BE(20);
-console.log(`Width: ${width}, Height: ${height}`);
